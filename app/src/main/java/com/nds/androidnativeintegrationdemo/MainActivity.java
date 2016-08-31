343ead3bbb3f61dbdb4f2fd77c76bbb4296c8899
Howard Tam <howardthk@gmail.com>
2016-08-31 11:12:46 +0800

    Apply the android library

--- app/src/main/java/com/nds/androidnativeintegrationdemo/MainActivity.java
+++ app/src/main/java/com/nds/androidnativeintegrationdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nds.androidnativeintegrationdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_go_lib).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.nds.nativelibrary.MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

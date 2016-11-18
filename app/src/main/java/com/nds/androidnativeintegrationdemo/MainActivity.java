2be6fca867838d76e0b590861f5a45da5f368279
Howard Tam <howardthk@gmail.com>
2016-11-18 13:02:02 +0800

    update integration method

--- app/src/main/java/com/nds/androidnativeintegrationdemo/MainActivity.java
+++ app/src/main/java/com/nds/androidnativeintegrationdemo/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import hk.redpages.servicetab.ServiceTabCallback;
+import hk.redpages.servicetab.ServiceTabLibrary;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_go_lib).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, com.nds.nativelibrary.MainActivity.class);
-                startActivity(intent);
+                /*Intent intent = new Intent(MainActivity.this, com.nds.nativelibrary.MainActivity.class);
+                startActivity(intent);*/
+                ServiceTabLibrary.getInstance().setup(getApplicationContext(), new ServiceTabCallback() {
+                    @Override
+                    public void onMotherLodeDeepLinkClicked(String link) {
+
+                    }
+                }).init();
+                /*Intent intent = new Intent(getApplicationContext(), hk.redpages.servicetab.activity.MainActivity.class);
+                startActivity(intent);*/
             }
         });
     }
